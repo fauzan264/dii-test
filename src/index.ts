5b66548a644a609005f602c24e254537ad7d2f2d
Fauzan <fauzan.c4@gmail.com>
2025-12-10 23:33:19 +0700

    init project

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app: Express = express();
+const port = 4001;
+
+// app.use(mainRouter);
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  res.status(500).json({
+    success: false,
+    message: error?.isExpose ? error?.message : "Internal server error",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`⚡️ Server is running on port ${port}`);
+});

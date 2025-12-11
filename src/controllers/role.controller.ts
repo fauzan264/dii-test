eb995eaa557f1f2a3d357e166454b326fe12259d
Fauzan <fauzan.c4@gmail.com>
2025-12-11 15:18:42 +0700

    feat: add endpoint update role

--- src/controllers/role.controller.ts
+++ src/controllers/role.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createRoleService } from "../services/role.service";
+import { createRoleService, updateRoleService } from "../services/role.service";
 
 export const createRoleController = async (req: Request, res: Response) => {
   const { name, description } = req.body;
@@ -9,6 +9,19 @@ export const createRoleController = async (req: Request, res: Response) => {
 	res.status(201).json({
 		success: true,
 		message: "Role created successfully",
+		data: role
+	})
+}
+
+export const updateRoleController = async (req: Request, res: Response) => {
+  const { name, description } = req.body;
+	const { id } = req.params;
+
+  const role = await updateRoleService({id: String(id), name, description});
+
+	res.status(200).json({
+		success: true,
+		message: "Role updated successfully",
 		data: role
 	})
 }
--- src/routes/role.router.ts
+++ src/routes/role.router.ts
@@ -1,12 +1,12 @@
 import Router from "express";
 import { validateYup } from "../middlewares/validateYup";
 import { createRoleSchema, deleteRoleSchema, updateRoleSchema } from "../middlewares/ validators/role.validator";
-import { createRoleController } from "../controllers/role.controller";
+import { createRoleController, updateRoleController } from "../controllers/role.controller";
 
 const roleRouter = Router()
 
 roleRouter.post("/", validateYup(createRoleSchema), createRoleController);
-// roleRouter.put("/", validateYup(updateRoleSchema), updateRoleController);
-// roleRouter.delete("/", validateYup(deleteRoleSchema), deleteRoleController);
+roleRouter.put("/:id", validateYup(updateRoleSchema), updateRoleController);
+// roleRouter.delete("/:id", validateYup(deleteRoleSchema), deleteRoleController);
 
 export default roleRouter;

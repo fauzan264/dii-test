d9b03b0a45438701fabf14645af0053035dae14f
Fauzan <fauzan.c4@gmail.com>
2025-12-12 04:30:52 +0700

    fix: add jwt verify

--- src/routes/menu.router.ts
+++ src/routes/menu.router.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { createMenuController, deleteMenuController, detailMenuController, getListMenuController, updateMenuController } from "../controllers/menu.controller";
 import { validateYup } from "../middlewares/validateYup";
 import { createMenuSchema, updateMenuSchema } from "../middlewares/validators/menu.validator";
+import { jwtVerify } from "../middlewares/jwt.verify";
 
 const menuRouter = Router();
 
-menuRouter.post("/", validateYup(createMenuSchema), createMenuController);
-menuRouter.put("/:id", validateYup(updateMenuSchema), updateMenuController);
-menuRouter.delete("/:id", deleteMenuController);
-menuRouter.get("/:id", detailMenuController);
-menuRouter.get("/", getListMenuController);
+menuRouter.post("/", jwtVerify, validateYup(createMenuSchema), createMenuController);
+menuRouter.put("/:id", jwtVerify, validateYup(updateMenuSchema), updateMenuController);
+menuRouter.delete("/:id", jwtVerify, deleteMenuController);
+menuRouter.get("/:id", jwtVerify, detailMenuController);
+menuRouter.get("/", jwtVerify, getListMenuController);
 
 export default menuRouter;
--- src/routes/role.router.ts
+++ src/routes/role.router.ts
@@ -2,15 +2,16 @@ import Router from "express";
 import { validateYup } from "../middlewares/validateYup";
 import { addRoleMenusSchema, createRoleSchema, deleteRoleMenusSchema, updateRoleSchema } from "../middlewares/validators/role.validator";
 import { addRoleMenusController, createRoleController, deleteRoleController, deleteRoleMenusController, detailRoleController, getListRoleController, updateRoleController } from "../controllers/role.controller";
+import { jwtVerify } from "../middlewares/jwt.verify";
 
 const roleRouter = Router()
 
-roleRouter.post("/", validateYup(createRoleSchema), createRoleController);
-roleRouter.put("/:id", validateYup(updateRoleSchema), updateRoleController);
-roleRouter.delete("/:id", deleteRoleController);
-roleRouter.get("/:id", detailRoleController);
-roleRouter.get("/", getListRoleController);
-roleRouter.post("/:id/menus", validateYup(addRoleMenusSchema), addRoleMenusController);
-roleRouter.delete("/:id/menus", validateYup(deleteRoleMenusSchema), deleteRoleMenusController);
+roleRouter.post("/", jwtVerify, validateYup(createRoleSchema), createRoleController);
+roleRouter.put("/:id", jwtVerify, validateYup(updateRoleSchema), updateRoleController);
+roleRouter.delete("/:id", jwtVerify, deleteRoleController);
+roleRouter.get("/:id", jwtVerify, detailRoleController);
+roleRouter.get("/", jwtVerify, getListRoleController);
+roleRouter.post("/:id/menus", jwtVerify, validateYup(addRoleMenusSchema), addRoleMenusController);
+roleRouter.delete("/:id/menus", jwtVerify, validateYup(deleteRoleMenusSchema), deleteRoleMenusController);
 
 export default roleRouter;
